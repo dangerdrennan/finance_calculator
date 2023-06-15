040ae6b3a628e214d0015d6741ee77dee1b4dd96
Phillip Drennan <pdrennan90@gmail.com>
2023-06-15 11:58:52 -0800

    added model field for the % contribution

--- finance_calculator/retirement_calculator/models.py
+++ finance_calculator/retirement_calculator/models.py
@@ -7,13 +7,21 @@ class Profile(models.Model):
     retirement_age = models.IntegerField()
     current_salary = models.FloatField()
     current_savings = models.FloatField()
+    percent_contribution = models.FloatField()
     rate_of_return = models.FloatField()
 
     def calculate_years_to_retirement(self):
         return self.retirement_age - self.current_age
     
     def calculate_return(self, years):
-        return self.current_savings * ((1 + self.rate_of_return) ** years) - self.current_savings
+        # normalize the percentages for calculation
+        percent_contribution = self.percent_contribution / 100
+        rate_of_return = self.rate_of_return / 100
+        # calculate annual contribution
+        annual_contribution = self.current_salary * percent_contribution
+        # return future value of the series formula
+        return (self.current_savings * (1 + rate_of_return) ** years) + (annual_contribution * (((1 + rate_of_return) ** years) - 1) / rate_of_return)
+
 
     class Meta:
         verbose_name = 'Profile'
