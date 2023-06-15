9ca2741e755028236a83f9e7798f3685e38168e8
Phillip Drennan <pdrennan90@gmail.com>
2023-06-15 09:14:59 -0800

    added a views.py function for the form

--- finance_calculator/retirement_calculator/views.py
+++ finance_calculator/retirement_calculator/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 from .models import Profile
 from .forms import ProfileForm
+
+def retirement_calulator(request):
+    if request.method =='POST':
+        form = ProfileForm(request.POST)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save
+        
+            years_to_retirement = profile.calculate_years_to_retirement()
+            end_balance = profile.calculate_return(years_to_retirement)
+            return render(request, 'calculator_result.html', {'retirement_savings': end_balance})
+        else:
+            form = ProfileForm()
+        return render(request, 'calculator.html', {'form':form})
