030f996024e7ac6d329b47976ceeef62dee2eac2
Phillip Drennan <pdrennan90@gmail.com>
2023-06-15 15:07:58 -0800

    added field in model for salary increases

--- finance_calculator/retirement_calculator/models.py
+++ finance_calculator/retirement_calculator/models.py
@@ -3,24 +3,37 @@ from django.conf import settings
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    current_age = models.IntegerField()
-    retirement_age = models.IntegerField()
-    current_salary = models.FloatField()
-    current_savings = models.FloatField()
-    percent_contribution = models.FloatField()
-    rate_of_return = models.FloatField()
+    current_age = models.IntegerField(default=33)
+    retirement_age = models.IntegerField(default=65)
+    current_salary = models.FloatField(default=87477)
+    current_savings = models.FloatField(default=10330)
+    percent_contribution = models.FloatField(default=20)
+    rate_of_return = models.FloatField(default=4)
+    projected_yearly_salary_increase = models.FloatField(default=3.25)
 
     def calculate_years_to_retirement(self):
         return self.retirement_age - self.current_age
     
     def calculate_return(self, years):
-        # normalize the percentages for calculation
+        # Normalize the percentages for calculation
         percent_contribution = self.percent_contribution / 100
         rate_of_return = self.rate_of_return / 100
-        # calculate annual contribution
-        annual_contribution = self.current_salary * percent_contribution
-        # return future value of the series formula
-        return (self.current_savings * (1 + rate_of_return) ** years) + (annual_contribution * (((1 + rate_of_return) ** years) - 1) / rate_of_return)
+        yearly_salary_increase = self.projected_yearly_salary_increase / 100
+
+        # Initial conditions
+        total = self.current_savings
+        salary = self.current_salary
+
+        for year in range(years):
+            # Add this year's contribution
+            total += salary * percent_contribution
+
+            # Grow the total amount and salary by their respective rates
+            total *= (1 + rate_of_return)
+            salary *= (1 + yearly_salary_increase)
+
+        return total
+
 
 
     class Meta:
