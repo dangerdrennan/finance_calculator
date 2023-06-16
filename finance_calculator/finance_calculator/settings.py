ac5445d415d9e89421ef99528de81ece16dc7704
Phillip Drennan <pdrennan90@gmail.com>
2023-06-16 10:45:34 -0800

    added more styling to calculator

--- finance_calculator/finance_calculator/settings.py
+++ finance_calculator/finance_calculator/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'retirement_calculator.apps.RetirementCalculatorConfig',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
