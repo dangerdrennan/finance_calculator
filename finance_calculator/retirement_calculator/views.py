ae4e153c29f78170ac2720ac6207c24b68d7cdb0
Phillip Drennan <pdrennan90@gmail.com>
2023-06-20 10:42:26 -0800

    calculated total appears on same page

--- finance_calculator/retirement_calculator/views.py
+++ finance_calculator/retirement_calculator/views.py
@@ -3,16 +3,19 @@ from .models import Profile
 from .forms import ProfileForm
 
 def retirement_calulator(request):
-    if request.method =='POST':
+    retirement_savings = None
+
+    if request.method == 'POST':
         form = ProfileForm(request.POST)
         if form.is_valid():
             profile = form.save(commit=False)
-            profile.user = request.user
-            profile.save
-        
             years_to_retirement = profile.calculate_years_to_retirement()
-            end_balance = profile.calculate_return(years_to_retirement)
-            return render(request, 'retirement_calculator/calculator_result.html', {'retirement_savings': end_balance})
+            retirement_savings = profile.calculate_return(years_to_retirement)
+            ## saves user. Uncomment after log-in feature implemented
+            # profile.user = request.user
+            # profile.save()
     else:
         form = ProfileForm()
-    return render(request, 'retirement_calculator/calculator.html', {'form':form})
+
+    context = {'form': form, 'retirement_savings': retirement_savings}
+    return render(request, 'retirement_calculator/calculator.html', context)
